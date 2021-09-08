18aa67fda9577c524aa7bc2b1df92a76765db17c
Lime5005 <zlp5005@gmail.com>
2021-09-08 16:37:50 +0200

    Add 6 urls tests

---
+++ src/test/java/com/lime/TestingWebApplicationTest.java
@@ -0,0 +1,66 @@
+package com.lime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestingWebApplicationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getAllEmails_givenCity_shouldReturnAList() throws Exception {
+        this.mockMvc.perform(get("/communityEmail?city=Culver")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("drk@email.com")));
+    }
+
+    @Test
+    public void getAllPhones_givenStation_shouldReturnAList() throws Exception {
+        this.mockMvc.perform(get("/phoneAlert?firestation=1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("841-874-6512")));
+    }
+
+    @Test
+    public void getPersonsAndStations_givenAddress_shouldReturnAnObj() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        this.mockMvc.perform(get("/fire?address=112 Steppes Pl")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.stationNumbers").value(list));
+    }
+
+    @Test
+    public void getPersons_givenStation_shouldReturnPersons() throws Exception {
+        this.mockMvc.perform(get("/firestation?stationNumber=1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.total_adult").value(5))
+                .andExpect(jsonPath("$.total_child").value(1));
+    }
+
+    @Test
+    public void getPerson_givenName_shouldReturnPersonInfo() throws Exception {
+        this.mockMvc.perform(get("/personInfo?firstName=Jamie&lastName=Peters")).andDo(print()).andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(39))
+                .andExpect(jsonPath("$.address", is("908 73rd St")));
+    }
+
+    @Test
+    public void getChildren_givenAddress_shouldReturnChildAndParents() throws Exception {
+        this.mockMvc.perform(get("/childAlert?address=1509 Culver St")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.children[0].age").value(9))
+                .andExpect(jsonPath("$.children[1].age").value(4));
+    }
+
+}
